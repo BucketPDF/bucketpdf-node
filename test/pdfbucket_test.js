f78eb763f8014db6bb561ad31fb19e86aaafb34f
gualopezb <gustavo@kommit.co>
2016-06-27 14:12:09 -0500

    Change sign per encrypt action

--- test/pdfbucket_test.js
+++ test/pdfbucket_test.js
@@ -1,62 +1,63 @@
 var chai   = require('chai'),
     expect = chai.expect,
     assert = chai.assert,
-    Signer = require('./../lib/pdfbucket');
+    PDFBucket = require('./../lib/pdfbucket');
 
-describe('Testing create Signer with valid parameters', function() {
-  var s;
+describe('Testing create PDFBucket with valid parameters', function() {
+  var pdfBucket;
   beforeEach(function() {
-    s = new Signer({apiKey: "PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5", apiSecret: "HieMN8dvi5zfSbKvqxKccxDo3LozqOIrY59U/jrZY54="});
+    pdfBucket = new PDFBucket({apiKey: "PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5", apiSecret: "HieMN8dvi5zfSbKvqxKccxDo3LozqOIrY59U/jrZY54="});
   });
 
-  it('should return a Signer object with valid parameters', function() {
-    assert.typeOf(s, "object");
+  it('should return a PDFBucket object with valid parameters', function() {
+    assert.typeOf(pdfBucket, "object");
   });
 
-  it('should generate signed url passing valid parameters', function() {
-    var signedUrl = s.generateUrl("https://www.joyent.com/", "landscape", "A4", "2px", "0.7");
-    assert.isString(signedUrl);
+  it('should generate encrypted url passing valid parameters', function() {
+    var encryptedUrl = pdfBucket.generateUrl("https://www.joyent.com/", "landscape", "A4", "2px", "0.7");
+    assert.isString(encryptedUrl);
+    console.log(encryptedUrl);
   });
 
   it('should throws invalid uri passed in when uri is blank', function() {
     assert.throws(function() {
-      var signedUrl = s.generateUrl("    ", "landscape", "A4", "2px", "0.7");
+      var encryptedUrl = pdfBucket.generateUrl("    ", "landscape", "A4", "2px", "0.7");
     }, "Invalid uri value, must be not blank");
   });
 
   it('should throws invalid orientation passed in when orientation is neither portrait or landscape', function() {
     assert.throws(function() {
-      var signedUrl = s.generateUrl("https://www.joyent.com/", "something", "A4", "2px", "0.7");
+      var encryptedUrl = pdfBucket.generateUrl("https://www.joyent.com/", "something", "A4", "2px", "0.7");
     }, "Invalid orientation value, must be portrait or landscape");
   });
 
   it('should throws invalid pageSize passed in when pageSize is neither A4 or Letter', function() {
     assert.throws(function() {
-      var signedUrl = s.generateUrl("https://www.joyent.com/", "landscape", "something", "2px", "0.7");
+      var encryptedUrl = pdfBucket.generateUrl("https://www.joyent.com/", "landscape", "something", "2px", "0.7");
     }, "Invalid pageSize value, must be A4 or Letter");
   });
 
-  it('should return a Signer object with valid parameters through environment variables', function() {
+  it('should return a pdfBucket object with valid parameters through environment variables', function() {
     process.env["PDF_BUCKET_API_KEY"]    = "PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5";
     process.env["PDF_BUCKET_API_SECRET"] = "HieMN8dvi5zfSbKvqxKccxDo3LozqOIrY59U/jrZY54=";
-    var s = new Signer();
+    var pdfBucket = new PDFBucket();
     process.env["PDF_BUCKET_API_KEY"]    = "";
     process.env["PDF_BUCKET_API_SECRET"] = "";
-    assert.typeOf(s, "object");
+    assert.typeOf(pdfBucket, "object");
   });
 });
 
-describe('Testing create Signer with invalid parameters', function() {
-  it('Signer() initialization should throws "bucket apiKey is required" if no apiKey is passed in', function() {
+describe('Testing create PDFBucket with invalid parameters', function() {
+  it('PDFBucket() initialization should throws "bucket apiKey is required" if no apiKey is passed in', function() {
     assert.throws(function() {
-      var s = new Signer({apiSecret: "HieMN8dvi5zfSbKvqxKccxDo3LozqOIrY59U/jrZY54="});
-      console.log(s);
+      var pdfBucket = new PDFBucket({apiSecret: "HieMN8dvi5zfSbKvqxKccxDo3LozqOIrY59U/jrZY54="});
+      console.log(pdfBucket);
     }, "bucket apiKey is required");
   });
 
-  it('Signer() initialization should throws "bucket apiSecret is required" if no apiSecret is passed in', function() {
+  it('PDFBucket() initialization should throws "bucket apiSecret is required" if no apiSecret is passed in', function() {
     assert.throws(function() {
-      var s = new Signer({apiKey: "PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5"});
+      var pdfBucket = new PDFBucket({apiKey: "PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5"});
     }, "bucket apiSecret is required");
   });
 
